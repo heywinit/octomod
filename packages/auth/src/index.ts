51e8e9d7f4271a4a1477450243f891dfbf374032
winit <heywinit@gmail.com>
2025-11-02 08:28:17 +0530

    chore: bts project setup. full nextjs

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,24 @@
+import { nextCookies } from 'better-auth/next-js';
+import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@octomod/db";
+import * as schema from "@octomod/db/schema/auth";
+
+export const auth = betterAuth<BetterAuthOptions>({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+
+		schema: schema,
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	emailAndPassword: {
+		enabled: true,
+	},
+	socialProviders: {
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID!,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+		},
+	},
+  plugins: [nextCookies()]
+});
