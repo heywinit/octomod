6d8424cee2722873d67de675c2d710bdcb32cd19
winit <heywinit@gmail.com>
2025-12-17 19:29:42 +0530

    nitro for vercel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,9 +3,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from 'nitro/vite'
+
 
 export default defineConfig({
-	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
 	server: {
 		port: 3001,
 	},
